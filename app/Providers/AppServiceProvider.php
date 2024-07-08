cac01c39d0659b141921d3ca43a0fb08a5c747b2
Muhammad Naufal <me@falkia34.dev>
2024-07-08 13:48:27 +0700

    fix(route): Customise verify email url generator
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,9 @@ namespace App\Providers;
 
 use App\Models\Client;
 use App\Models\PersonalAccessClient;
+use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Laravel\Passport\Passport;
 
@@ -22,6 +25,16 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        VerifyEmail::createUrlUsing(function ($notifiable) {
+            return URL::temporarySignedRoute(
+                'auth.email-verification.verify',
+                Carbon::now()->addMinutes(config('auth.verification.expire', 60)),
+                [
+                    'id' => $notifiable->getKey(),
+                    'hash' => sha1($notifiable->getEmailForVerification()),
+                ]
+            );
+        });
         Passport::useClientModel(Client::class);
         Passport::usePersonalAccessClientModel(PersonalAccessClient::class);
         Passport::hashClientSecrets();
--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,7 @@ Route::group([
     Route::get('/register', 'DummyController')->name('register');
     Route::get('/login', 'DummyController')->name('login');
     Route::get('/logout', 'DummyController')->name('logout');
-    Route::get('/forgot-password', 'DummyController')->name('password.request');
     Route::get('/reset-password', 'DummyController')->name('password.reset');
     Route::get('/verify-email', 'DummyController')->name('verification.notice');
-    Route::get('/verify-email/{id}/{hash}', 'DummyController')->name('verification.verify');
     Route::get('/dashboard', 'DummyController')->name('dashboard');
 });
