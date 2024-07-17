403b04ec244cad07c241b9c8b8b61b337f6ae6df
Muhammad Naufal <me@falkia34.dev>
2024-07-17 23:06:48 +0700

    fix(route): Fix implicit model binding on channel route
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

--- routes/channels.php
+++ routes/channels.php
@@ -3,4 +3,4 @@
 use App\Broadcasting\ChatSessionChannel;
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('chat-sessions.{chat_session}', ChatSessionChannel::class);
+Broadcast::channel('chat-sessions.{chatSession}', ChatSessionChannel::class);
