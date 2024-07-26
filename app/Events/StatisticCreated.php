2c7395cc417bf782b7ef8c0af52f8c654491983c
Muhammad Naufal <me@falkia34.dev>
2024-07-26 13:42:08 +0700

    feat(event): Add statistic event
    Signed-off-by: Muhammad Naufal <me@falkia34.dev>

---
+++ app/Events/StatisticCreated.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Statistic;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class StatisticCreated
+{
+    use Dispatchable, SerializesModels;
+
+    /**
+     * Create a new event instance.
+     */
+    public function __construct(
+        public Statistic $statistic,
+    ) {}
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return array<int, \Illuminate\Broadcasting\Channel>
+     */
+    public function broadcastOn(): Channel
+    {
+        return new PrivateChannel('users.'.$this->statistic->user_id);
+    }
+
+    /**
+     * The event's broadcast name.
+     */
+    public function broadcastAs(): string
+    {
+        return 'statistic.created';
+    }
+}
